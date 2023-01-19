1a084f235d62ba62f6f9b65bd0ac5845c135627f
Admin <vedanth_21@gmail.com>
2023-01-19 08:23:18 +0530

    added new code to the file

--- Mongodb.sh
+++ Mongodb.sh
@@ -5,3 +5,7 @@ cp ${script_location}/Files/Mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
