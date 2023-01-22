4d18e79bb99e6d400d645fcd2b22b7a227bf53ce
Admin <vedanth_21@gmail.com>
2023-01-22 07:51:55 +0530

    Catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,7 @@ yum install nodejs -y
 # useradd roboshop
 mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
@@ -16,3 +17,4 @@ cp ${script_location}/Files/catalogue.service /etc/systemd/systems/catalogue.ser
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
