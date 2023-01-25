5e4697e3b03830f33390e42b9ed59587f90ba507
Admin <vedanth_21@gmail.com>
2023-01-25 16:57:23 +0530

    if condition

--- Frontend.sh
+++ Frontend.sh
@@ -5,33 +5,33 @@ yum install nginx -y
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
 
 systemctl enable nginx
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
 systemctl start nginx
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
 rm -rf /usr/share/nginx/html/*
 
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
 
 cd /usr/share/nginx/html
@@ -40,7 +40,7 @@ unzip /tmp/frontend.zip
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
 
 cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
@@ -49,5 +49,5 @@ systemctl restart nginx
 if [$? -eq 0]; then
   echo success
   else
-    each failure
+    echo failure
 fi
