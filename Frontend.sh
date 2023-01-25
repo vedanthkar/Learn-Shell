81286eff3b3dba5b702e9ac6d878f1ac4d60fcfb
Admin <vedanth_21@gmail.com>
2023-01-25 16:20:07 +0530

    Catalogue

--- Frontend.sh
+++ Frontend.sh
@@ -2,12 +2,22 @@ script_location=$(pwd)
 
 
 yum install nginx -y
+echo $?
+
 systemctl enable nginx
 systemctl stanrt nginx
 rm -rf /usr/share/nginx/html/*
+
+echo $?
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo $?
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo $?
+
 cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
+
+echo $?
--- catalogue.sh
+++ catalogue.sh
@@ -3,6 +3,7 @@ script_location=$(pwd)
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+
 useradd roboshop
 mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
